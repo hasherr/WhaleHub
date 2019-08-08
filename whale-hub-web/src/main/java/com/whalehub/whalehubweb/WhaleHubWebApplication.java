db40e126199c40b4142b7a6624f47aeaf4c6401c
Evan Klein <eklein@misoenergy.org>
2019-08-08 13:19:56 -0400

    Added backend

---
+++ whale-hub-web/src/main/java/com/whalehub/whalehubweb/WhaleHubWebApplication.java
@@ -0,0 +1,13 @@
+package com.whalehub.whalehubweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhaleHubWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhaleHubWebApplication.class, args);
+	}
+
+}
---
+++ whale-hub-web/src/test/java/com/whalehub/whalehubweb/WhaleHubWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.whalehub.whalehubweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WhaleHubWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
