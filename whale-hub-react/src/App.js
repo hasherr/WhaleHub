634ac0f5e15f7415ea996e5e327f2638deb04b0f
hasherr <eklein@gmail.com>
2019-08-12 12:47:08 -0400

    Routing set up

--- whale-hub-react/src/App.js
+++ whale-hub-react/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
 import Nav from './Nav/Nav';
+import MapPage from './MapPage/MapPage';
+import ReactDOM from 'react-dom'
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 
 
 function App() {
   return (
     <div className="App">
       <Nav></Nav>
+      <Router>
+        <div>
+          <Route path="/" component={MapPage} />
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ whale-hub-react/src/MapPage/MapPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+function MapPage() {
+  return (
+    <div className="mapPage">
+      <h2>Map</h2>
+    </div>
+  );
+}
+
+export default MapPage;
